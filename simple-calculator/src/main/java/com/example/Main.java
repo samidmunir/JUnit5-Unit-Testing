f4b5ab0694f4f4e3db9ecfe80ba6fa3e7b8905b7
Sami Munir <samidmunir@outlook.com>
2026-03-04 21:41:57 -0500

    creating first Java/Maven with JUnit5 dep for simple calculator testing

---
+++ simple-calculator/src/main/java/com/example/Main.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nSimple Calculator Test with JUnit5\n-----------------------------------\n");
+    }
+}
