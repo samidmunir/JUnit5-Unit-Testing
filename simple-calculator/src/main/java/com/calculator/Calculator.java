bdb99191e41ab525620c3b0ad67924ef60ff6aa2
Sami Munir <samidmunir@outlook.com>
2026-03-08 18:02:19 -0400

    cleaning up JUnit5 projects

--- simple-calculator/src/main/java/com/calculator/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.calculator;
-
-public class Calculator {
-    public int add(int a, int b) {
-        int sum = a + b;
-
-        return sum;
-    }
-}
--- simple-calculator/src/main/java/com/example/Main.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("\nSimple Calculator Test with JUnit5\n-----------------------------------\n");
-    }
-}
